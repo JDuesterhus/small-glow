9d3295346f86819550d59aa591d5323375d1b633
Jeremy Düsterhus <50998565+JDuesterhus@users.noreply.github.com>
2019-05-24 19:01:33 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,54 @@
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include <tlhelp32.h>
+
+DWORD PID;
+
+HANDLE GetHandle(const char* process) {
+	HANDLE hPID = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL);
+	PROCESSENTRY32 ProcEntry;
+	ProcEntry.dwSize = sizeof(ProcEntry);
+	do {
+		if (!strcmp(ProcEntry.szExeFile, process)) {
+			PID = ProcEntry.th32ProcessID;
+			CloseHandle(hPID);
+			return OpenProcess(PROCESS_ALL_ACCESS, FALSE, PID);
+		}
+	} while (Process32Next(hPID, &ProcEntry));
+	return 0;
+}
+
+DWORD GetModule(const char* module) {
+	HANDLE hModule = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, PID);
+	MODULEENTRY32 mEntry;
+	mEntry.dwSize = sizeof(mEntry);
+	while (Module32Next(hModule, &mEntry)) {
+		if (!strcmp(mEntry.szModule, module)) {
+			CloseHandle(hModule);
+			return (DWORD)mEntry.modBaseAddr;
+		}
+	}
+	return 0;
+}
+
+#define dwEntityList 0x4D04A94
+#define m_flDetectedByEnemySensorTime 0x3960
+#define m_bSpotted 0x93D
+//#define m_lifeState 0x25F
+//#define m_bDormant 0xED
+
+void __stdcall NoCRTMain() {
+	HANDLE Vidya = GetHandle("csgo.exe");
+	DWORD ClientDLL = GetModule("client_panorama.dll");
+	while (true) {
+		for (int i = 0; i <= 64; i++) {
+			DWORD EntityList;
+			const float glowtime = 134217722; //max
+			const bool radarspotted = true;
+			ReadProcessMemory(Vidya, (LPVOID)(ClientDLL + dwEntityList + i * 0x10), &EntityList, sizeof(EntityList), NULL);
+			WriteProcessMemory(Vidya, (LPVOID)(EntityList + m_flDetectedByEnemySensorTime), &glowtime, sizeof(glowtime), NULL);
+			WriteProcessMemory(Vidya, (LPVOID)(EntityList + m_bSpotted), &radarspotted, sizeof(radarspotted), NULL);
+		}
+		Sleep(100);
+	}
+}
